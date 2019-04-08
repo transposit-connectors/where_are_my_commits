9f809e5da82298b04683f42adb23d7bbdf99c839
Nina Yang <nina@transposit.com>
2019-04-08 22:46:58 +0000

    Commit from Transposit IDE

---
+++ src/WheresMyCommit.js
@@ -0,0 +1,10 @@
+(params) => {
+  return {
+    mission: "complete"
+  };
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://api-composition.transposit.com/references/js-operations
+ */
