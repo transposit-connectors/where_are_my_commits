03ed44b3da365dd9044b44baae1bf848125ffa81
Nina Yang <nina@transposit.com>
2019-04-08 22:46:29 +0000

    Initial service boilerplate

---
+++ src/hello_world.js
@@ -0,0 +1,17 @@
+(params) => {
+  return [
+    {
+      language: "english",
+      message: "Hello, world"
+    },
+    {
+      language: "spanish",
+      message: "Hola, mundo"
+    }
+  ];
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://api-composition.transposit.com/references/js-operations
+ */
