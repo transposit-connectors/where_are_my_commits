2a509bbc448f2a48d5e7b01a2853bafe80d803ce
Nina Yang <nina@transposit.com>
2019-04-09 00:57:41 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -1,22 +1,22 @@
 ({ http_event }) => {
   console.log(http_event);
-  var sha;
-  var shaArray = http_event.body.split("&");
-   shaArray.forEach((entry) => {
-    if (entry.startsWith("text=")) {
-      sha = entry.substring(5);
-    }
-  });
-  api.log(sha);
+//   var sha;
+//   var shaArray = http_event.body.split("&");
+//    shaArray.forEach((entry) => {
+//     if (entry.startsWith("text=")) {
+//       sha = entry.substring(5);
+//     }
+//   });
+//   api.log(sha);
   
-  var prodDeployEnabled = api.run("this.prodDeployEnabled")[0];
-  var deployedCommits = api.run("this.GetDeployedCommits");
-  var commitAndEnv = api.run("this.FindCommit", {demoCommit: deployedCommits[0], 
-                                                stagingCommit: deployedCommits[1], 
-                                                prodCommit: deployedCommits[2], 
-                                                sha: sha});
+//   var prodDeployEnabled = api.run("this.prodDeployEnabled")[0];
+//   var deployedCommits = api.run("this.GetDeployedCommits");
+//   var commitAndEnv = api.run("this.FindCommit", {demoCommit: deployedCommits[0], 
+//                                                 stagingCommit: deployedCommits[1], 
+//                                                 prodCommit: deployedCommits[2], 
+//                                                 sha: sha});
   
-  var message = `This commit is on ${commitAndEnv.commitEnv}. Prod deploy is ${prodDeployEnabled}`;
+//   var message = `This commit is on ${commitAndEnv.commitEnv}. Prod deploy is ${prodDeployEnabled}`;
   
   // get the head of prod
   // get the head of staging -> see if the commit is in between those and if it is then it's on prod
@@ -27,7 +27,7 @@
   return {
     status_code: 200,
     headers: { "Content-Type": "application/json" },
-    body: {"message": message}
+    body: {text: message}
   };
 }
 
