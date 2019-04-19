f379ab129ff93d9a0635355230c56ba1f77a22ed
Nina Yang <nina@transposit.com>
2019-04-19 20:05:51 +0000

    Commit from Transposit IDE

---
+++ src/untitled_3.js
@@ -0,0 +1,11 @@
+(params) => {
+  api.run("this.MakeSlackMessage", {sha: "fd91dc860be055a81c293358acbb6f62ea1536cd"});
+  return {
+    mission: "complete"
+  };
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://docs.transposit.com/references/js-operations
+ */
