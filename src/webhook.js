717fe8370b5c1dd989b799497b91cdad88d85655
Nina Yang <nina@transposit.com>
2019-05-10 20:00:49 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -1,7 +1,15 @@
 ({http_event}) => {
+  var qs = require('qs.js');
   
   api.log(http_event);
   api.log(api.listUsers());
+  
+    
+  var parsedBody = qs.parse(http_event.body);
+  api.log(parsedBody);
+  var responseUrl = parsedBody.response_url;
+  var email = parsedBody.email;
+  var slackUser = parsedBody.user;
   
   
   // var sha;
