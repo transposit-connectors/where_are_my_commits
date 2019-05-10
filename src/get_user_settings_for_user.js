a71c74c3aa379f92090a76ebb74d62e8ea9c77cd
Nina Yang <nina@transposit.com>
2019-05-10 21:38:40 +0000

    Commit from Transposit IDE

--- src/get_user_settings_for_user.js
+++ src/get_user_settings_for_user.js
@@ -1,14 +1,20 @@
 (params) => {
+  
   var github = user_setting.get("github_username");
   if (!github) {
-    github = api.run("github.get_user_authenticated")[0].login;
+    if (api.isAuthed("github")) {
+      github = api.run("github.get_user_authenticated")[0].login;
     user_setting.put("github_username", github);
+    }
   }
   
   var slack = user_setting.get("slack_username");
   if (!slack) {
+    if (api.isAuthed("slack")) {
     slack = api.run("slack.get_users_profile")[0].profile.display_name;
-    user_setting.put("slack_username", slack);
+    user_setting.put("slack_username", slack);  
+    }
+    
   }
   
   return {github_username: github,
