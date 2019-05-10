7c514756c3e9ab5ac9083111237857c758da5b2f
Nina Yang <nina@transposit.com>
2019-05-10 21:13:22 +0000

    Commit from Transposit IDE

--- src/FindUser.js
+++ src/FindUser.js
@@ -5,7 +5,7 @@
   api.listUsers().forEach(user => {
     var settings = api.run("this.get_user_settings_for_user", {}, {asUser: user.email})[0];
     api.log(settings);
-  	if (settings.slack_username === username) {
+  	if (settings.slack_username && settings.slack_username === username) {
       foundUserEmail = user.email;
     }
   });
