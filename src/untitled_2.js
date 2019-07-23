e43beaa198e68a37385d4039390acc1d61c4c7de
Nina Yang <nina@transposit.com>
2019-07-23 17:04:30 +0000

    Commit from Transposit IDE

--- src/untitled_2.js
+++ src/untitled_2.js
@@ -16,20 +16,43 @@
     
   }
   
+  var fun = user_setting.get("fun_messages");
+  
+  var lineBreak = "\n--------------------------------------------\n";
+  
   // Get commits and find commit map
-  var allMessages = "";
+  var allMessages = lineBreak;
   var commitMap = api.run("this.GetRecentCommitMap")[0];
+  var numCommits = 0;
   Object.keys(commitMap).forEach(sha => {
     var commit = commitMap[sha];
   
-    if (commit.author && commit.author.login == github) {
+    // For some commits the author is null (and we only have their email)
+    // I am choosing to not handle that case right now.
+    if (commit.author && commit.author.login == username) {
+      numCommits += 1;
       api.log(commit);
       var newMessage = api.run("this.MakeSlackMessage", {sha: sha, commitMap: commitMap})[0];
       allMessages += newMessage;
-      allMessages += "\n";
-      allMessages += "--------------------------------------------"
+      allMessages += lineBreak;
     }
-  })
+  });
+  
+  if (numCommits == 0) {
+    var commits = api.run("github.list_commits", {owner: "transposit", repo: "transposit", author: username}, {limit: 5});
+    var emoji = fun ? ":iggy-party:" : ""
+	allMessages += "Your last 5 commits are:" + lineBreak;
+    commits.forEach(commit => {
+      allMessages += `${commit.commit.message}${lineBreak}`;
+    });
+    allMessages += `And they're all on Prod!${emoji}`
+  }
+  
+  var prodDeployEnabled = api.run("this.prodDeployEnabled")[0];
+  if (prodDeployEnabled) {
+  var emoji = fun ? ":sunny:" : ""
+      allMessages += `\n ${emoji}prod deployment is enabled${emoji} \n`;
+  }
   
   var body = {text: allMessages};
   
