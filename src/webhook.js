61c333559fba65613f954f2765f53d97fe9ce863
Nina Yang <nina@transposit.com>
2019-08-14 16:30:10 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -2,36 +2,33 @@
   
   api.log(http_event);
   
-    
-  var parsedBody = http_event.parsed_body;
-  var response_url = parsedBody.response_url;
-  var email = parsedBody.text;
-  var slackUser = parsedBody.user_name;
-  var runningAsSomeoneElse = false;
-  
-  var userId = parsedBody.user_id;
-  if (parsedBody.text && parsedBody.text.startsWith("<@")) {
-    runningAsSomeoneElse = true;
-    userId = parsedBody.text.split("|")[0].substring(2);
-  }
-
-
-  var foundUser = api.user({type: "slack", userId: userId, workspaceId: parsedBody.team_id});
-
-  api.log(foundUser);
-  
-  if (!foundUser) {
-    var text = runningAsSomeoneElse ? `Sorry, <@${userId}> hasn't set up this app. You can ping them to add their crednetials here: ${env.getBuiltin().appUrl}`
-    : "Please set up this app! " + env.getBuiltin().appUrl;
-      return {
-    status_code: 200,
-    headers: {"Content-Type": "application/json"},
-    body: {text: text}
-  };
-  }
-  
-  // api.log(sha);
   setImmediate(() => {
+    var parsedBody = http_event.parsed_body;
+    var response_url = parsedBody.response_url;
+    var email = parsedBody.text;
+    var slackUser = parsedBody.user_name;
+    var runningAsSomeoneElse = false;
+
+    var userId = parsedBody.user_id;
+    if (parsedBody.text && parsedBody.text.startsWith("<@")) {
+      runningAsSomeoneElse = true;
+      userId = parsedBody.text.split("|")[0].substring(2);
+    }
+
+
+    var foundUser = api.user({type: "slack", userId: userId, workspaceId: parsedBody.team_id});
+
+    api.log(foundUser);
+
+    if (!foundUser) {
+      var text = runningAsSomeoneElse ? `Sorry, <@${userId}> hasn't set up this app. You can ping them to add their crednetials here: ${env.getBuiltin().appUrl}`
+      : "Please set up this app! " + env.getBuiltin().appUrl;
+        return {
+      status_code: 200,
+      headers: {"Content-Type": "application/json"},
+      body: {text: text}
+    };
+    }
   	api.run("this.HandleSlackResponse", {response_url : response_url}, {asUser: foundUser.id});  
   });
   
