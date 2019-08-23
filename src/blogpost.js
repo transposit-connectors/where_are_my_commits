098efaa24d0d071a653313c1b31a7ff5e6a390b2
Nina Yang <nina@transposit.com>
2019-08-23 01:17:36 +0000

    Commit from Transposit IDE

--- src/blogpost.js
+++ src/blogpost.js
@@ -1,13 +1,14 @@
 (params) => {
   var body = params.untitled1;
   body.channel = "CK4K2AK4N";
+  // body.text = "Getting commits for <@jplace>. One moment please..."
   api.run("slack.post_chat_message", {$body: body})
   return {
     mission: "complete"
   };
 }
 
-/*
+/*fo/
  * For sample code and reference material, visit
  * https://www.transposit.com/docs/references/js-operations
  */
