b636d754c3234583d4cdb13a52080588aad9ad39
Nina Yang <nina@transposit.com>
2019-08-23 22:42:36 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -2,33 +2,33 @@
   
   // api.log(http_event);
   
-  setImmediate(() => {
-    var parsedBody = http_event.parsed_body;
-    var response_url = parsedBody.response_url;
-    var email = parsedBody.text;
-    var slackUser = parsedBody.user_name;
-    var runningAsSomeoneElse = false;
+//   setImmediate(() => {
+//     var parsedBody = http_event.parsed_body;
+//     var response_url = parsedBody.response_url;
+//     var email = parsedBody.text;
+//     var slackUser = parsedBody.user_name;
+//     var runningAsSomeoneElse = false;
 
-    var userId = parsedBody.user_id;
-    if (parsedBody.text && parsedBody.text.startsWith("<@")) {
-      runningAsSomeoneElse = true;
-      userId = parsedBody.text.split("|")[0].substring(2);
-    }
+//     var userId = parsedBody.user_id;
+//     if (parsedBody.text && parsedBody.text.startsWith("<@")) {
+//       runningAsSomeoneElse = true;
+//       userId = parsedBody.text.split("|")[0].substring(2);
+//     }
 
 
-    var foundUser = api.user({type: "slack", userId: userId, workspaceId: parsedBody.team_id});
+//     var foundUser = api.user({type: "slack", userId: userId, workspaceId: parsedBody.team_id});
 
-    api.log(foundUser);
+//     api.log(foundUser);
 
-    if (!foundUser) {
-      var text = runningAsSomeoneElse ? `Sorry, <@${userId}> hasn't set up this app. You can ping them to add their credentials here: ${env.getBuiltin().appUrl}`
-      : "Please set up this app! " + env.getBuiltin().appUrl;
+//     if (!foundUser) {
+//       var text = runningAsSomeoneElse ? `Sorry, <@${userId}> hasn't set up this app. You can ping them to add their credentials here: ${env.getBuiltin().appUrl}`
+//       : "Please set up this app! " + env.getBuiltin().appUrl;
       
       
-      api.run("slack_webhook.post_to_response_url", {response_url: response_url, post_body: {text: text}});
-    }
-  	api.run("this.HandleSlackResponse", {response_url : response_url}, {asUser: foundUser.id});  
-  });
+//       api.run("slack_webhook.post_to_response_url", {response_url: response_url, post_body: {text: text}});
+//     }
+//   	api.run("this.HandleSlackResponse", {response_url : response_url}, {asUser: foundUser.id});  
+//   });
   
   return {
     status_code: 200,
